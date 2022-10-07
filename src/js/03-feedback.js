59d237558549e48ba44fe2e66279faddfa483a2c
Nataly Siganova <siganova.tihanskaya@gmail.com>
2022-10-07 20:14:02 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -25,6 +25,7 @@ refs.formFeedback.addEventListener('submit', (event) => {
     
     event.currentTarget.reset();
     localStorage.removeItem(KEY);
+    formData = {};
 });
 
 function populateForm(){
@@ -37,7 +38,3 @@ function populateForm(){
     }
 }
 
-// Write a function that formats a date in such format "YYYY/MM/DD HH:mm".
-
-// console.log(formatDate(new Date("6/15/2019 09:15:00"))); // "2019/06/15 09:15"
-// console.log(formatDate(new Date())); // gets current local times
